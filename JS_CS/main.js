ab0399f0ce358ddea77226133e6d5f6a93e699dd
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2019-04-13 18:17:39 -0400

    13apr

---
+++ JS_CS/main.js
@@ -0,0 +1,108 @@
+
+const name = "ashwin";
+const age = 30;
+
+//concatenation
+console.log('My name is ashwin '+name+" and I am "+age);
+
+console.log(`My name is ${name} and im ${age} `);
+
+const s = "tech,computer,it,code";
+
+console.log(s.split(","))
+
+const fruits = ['apple','oranges','pears'];
+
+fruits[3] = 'grapes';
+
+fruits.push('mangos','banana');
+
+fruits.unshift('strwberries')
+fruits.pop()
+console.log(Array.isArray(fruits))
+
+console.log(fruits.indexOf('apple'))
+
+console.log(fruits)
+
+const person = {
+    firstName: 'Ashwin',
+    lastName : 'Muthiah',
+    age:30,
+    hobbies:['music','movies','sports'],
+    address:{
+        street:'110 main st',
+        city:'Boston',
+        states:'MA'
+    }
+}
+
+console.log(person.firstName);
+ person.address.city = 'Roxbury'
+ console.log(person)
+
+
+const todos = [
+    {
+        id:1,
+        text:'Take trash out',
+        isComplete:true
+    },
+    {
+        id:2,
+        text:'Boss meeting',
+        isComplete:false
+    },
+    {
+        id:3,
+        text:'Dentist',
+        isComplete:true
+    }
+ ]
+
+console.log(todos[1].text);
+
+const todoJSON = JSON.stringify(todos);
+console.log(todoJSON)
+
+// for loop
+
+for(let i=0;i<10;i++){
+    console.log(i)
+}
+
+// while 
+let i=0;
+while(i<10){
+    console.log(i)
+    i++;
+}
+
+
+for (let i=0;i<todos.length;i++){
+    console.log(todos[i])
+}
+
+for (let todo of todos){
+    console.log(todo.text);
+    break;
+}
+
+// for each,map,filter
+
+const a = todos.forEach(function(todo) {
+    console.log(todo.text);
+
+});
+
+const b = todos.map(function(todo) {
+    return todo.text;
+});
+
+console.log(b)
+
+const c = todos.filter(function(todo){
+    return todo.isComplete === false;
+});
+
+console.log(c)
