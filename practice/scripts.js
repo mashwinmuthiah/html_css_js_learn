9cc446504d3dd8bffb56bb9e2e1cd74b9db0c54e
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2019-05-09 13:14:51 -0400

    scripts: 30 min into class

--- practice/scripts.js
+++ practice/scripts.js
@@ -11,7 +11,57 @@ var ASHWIN;
 ashwin = 10;
 console.log(ASHWIN,ashwin);
 
-//adding numbers
+//arithmetic operation numbers
 var sum = 10+5;
 console.log(sum);
+var deff = 10-5;
+console.log(deff);
+var myvar = 5;
+myvar++
+console.log(myvar);
 
+//float point numbers
+var mydec = 5.544545544
+console.log(mydec);
+
+//plus equals operatior
+ a += 12;
+ console.log(a);
+ a-= 5;
+ console.log(a);
+
+ //variables 
+ var mystring = "This is a javascript string";
+ console.log(mystring);
+
+var mystr = "This is a \"double quoted \" string inside a string";
+console.log(mystr); 
+mystr = 'This is "double quotes " string';
+console.log(mystr);4
+
+/*
+Code	Output
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\r	carriage return
+\t	tab
+\b	backspace
+\f	form feed
+*/
+
+ var myvar2 = "ashwin\n\fmuthiah\nmurugappan"
+ console.log(myvar2);
+
+ //concatenation of strings
+
+ var mystr = "This is the start " + "This is the end";
+ console.log(mystr);
+ var outstr = "I come first ";
+ outstr += "I come last"
+ console.log(outstr); 
+
+ var fname = "ashwin";
+ var lname = "murugappan";
+ console.log("my first name is "+fname+" and my last name is "+lname);
