17fc955164c7f9ef603c93ea82ef0bccab26202e
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2019-05-09 12:47:43 -0400

    script : link

---
+++ practice/scripts.js
@@ -0,0 +1,17 @@
+/* This is a comment */
+
+//declaration of variables
+var a;
+var b = 2;
+a = 4;
+b = a;
+console.log(a,b);
+
+var ASHWIN;
+ashwin = 10;
+console.log(ASHWIN,ashwin);
+
+//adding numbers
+var sum = 10+5;
+console.log(sum);
+
